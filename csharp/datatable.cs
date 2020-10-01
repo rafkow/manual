d9d70ec4fa93253ebb281266ad70bf47dc189422
Rafał Kowalewski <rkowalewski@pl.indorama.net>
2020-10-01 22:02:47 +0200

    update_01/10/2020

--- csharp/datatable.cs
+++ csharp/datatable.cs
@@ -1,1 +1,11 @@
-install-package jquery.datatables
+PM> install-package jquery.datatables
+
+BundleConfig.cs
+bundles.Add(new ScriptBundle("~/bundles/datatables").Include(
+                        "~/Scripts/DataTables/jquery.datatables.js"
+                        , "~/Scripts/DataTables/dataTables.bootstrap.min.js"
+                        ));
+
+bundles.Add(new StyleBundle("~/Content/datatables").Include(
+    "~/Content/DataTables/css/dataTables.bootstrap.css"
+          ));
