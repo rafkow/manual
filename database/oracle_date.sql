97db009ea27e724eaaf26b6ba7892396bcc98778
Rafał Kowalewski <rkowalewski@pl.indorama.net>
2021-03-16 21:00:42 +0100

    update_16/03/2021

--- database/oracle_date.sql
+++ database/oracle_date.sql
@@ -13,6 +13,7 @@ TRUNC(SYSDATE) - today
 select * from gb_enter_register where TRUNC(SYSDATE) = TRUNC(enter_date);
 
 TO_DATE( '31-Dec-1999 15:30:20 ',  'DD-Mon-YYYY HH24:MI:SS' )
+update gb_invitations set exp_date = to_date('31-12-2021', 'DD-MM-YYYY') where visit_type = 'stała';
 
 TO_CHAR( SYSDATE, 'HH24' ) hour,
 TO_CHAR( SYSDATE, 'MI' ) minute,
