c295434ce504c680cf1f2fa42ec9a29fa5f2d9cc
Rafał Kowalewski <rkowalewski@pl.indorama.net>
2021-05-31 21:32:13 +0200

    update_31/05/2021

--- database/MsSql.sql
+++ database/MsSql.sql
@@ -1,1 +1,3 @@
 Alter Table dbo.t_result_check_item Add Id Int Identity(1, 1)
+update dbo.Obecnosc set RogerDateOut = CONVERT(DATETIME, '2021-05-27 15:07:23', 120)  where ID = 58514;
+convert(DATETIME, date , x)  x - wzór daty lista wzorów na - http://www.sqlusa.com/bestpractices/datetimeconversion/
