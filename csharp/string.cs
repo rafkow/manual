8db81393ddff212a95edc047f768e98d630dc016
Rafał Kowalewski <rkowalewski@pl.indorama.net>
2021-07-18 21:38:03 +0200

    update_18/07/2021

--- csharp/string.cs
+++ csharp/string.cs
@@ -6,3 +6,7 @@ liniowy"
 $ - wstaw zmienne w string
 int liczba = 5;
 Console.WriteLine($"to jest brakująca {liczba} liczba");
+
+
+Console.WriteLine($"{DateTime.Now,  25} Hello World"); //       07/16/2021 06:59:32 Hello World
+Console.WriteLine($"{DateTime.Now, -25} Hello World"); // 07/16/2021 06:58:45       Hello World
