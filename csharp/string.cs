ba23fefe9f30f9e1694fff75d478322e3e87822b
Rafał Kowalewski <rkowalewski@pl.indorama.net>
2021-10-12 21:01:58 +0200

    update_12/10/2021

--- csharp/string.cs
+++ csharp/string.cs
@@ -10,3 +10,31 @@ Console.WriteLine($"to jest brakująca {liczba} liczba");
 
 Console.WriteLine($"{DateTime.Now,  25} Hello World"); //       07/16/2021 06:59:32 Hello World
 Console.WriteLine($"{DateTime.Now, -25} Hello World"); // 07/16/2021 06:58:45       Hello World
+
+// just two decimal places
+String.Format("{0:0.00}", 123.4567);      // "123.46"
+String.Format("{0:0.00}", 123.4);         // "123.40"
+String.Format("{0:0.00}", 123.0);         // "123.00"
+// max. two decimal places
+String.Format("{0:0.##}", 123.4567);      // "123.46"
+String.Format("{0:0.##}", 123.4);         // "123.4"
+String.Format("{0:0.##}", 123.0);         // "123"
+// max. two decimal places
+String.Format("{0:0.##}", 123.4567);      // "123.46"
+String.Format("{0:0.##}", 123.4);         // "123.4"
+String.Format("{0:0.##}", 123.0);         // "123"
+String.Format("{0:0,0.0}", 12345.67);     // "12,345.7"
+String.Format("{0:0,0}", 12345.67);       // "12,346"
+String.Format("{0:0.0}", 0.0);            // "0.0"
+String.Format("{0:0.#}", 0.0);            // "0"
+String.Format("{0:#.0}", 0.0);            // ".0"
+String.Format("{0:#.#}", 0.0);            // ""
+String.Format("{0,10:0.0}", 123.4567);    // "     123.5"
+String.Format("{0,-10:0.0}", 123.4567);   // "123.5     "
+String.Format("{0,10:0.0}", -123.4567);   // "    -123.5"
+String.Format("{0,-10:0.0}", -123.4567);  // "-123.5    "
+String.Format("{0:0.00;minus 0.00;zero}", 123.4567);   // "123.46"
+String.Format("{0:0.00;minus 0.00;zero}", -123.4567);  // "minus 123.46"
+String.Format("{0:0.00;minus 0.00;zero}", 0.0);        // "zero"
+tring.Format("{0:my number is 0.0}", 12.3);   // "my number is 12.3"
+String.Format("{0:0aaa.bbb0}", 12.3);          // "12aaa.bbb3"
--- database/MsSql.sql
+++ database/MsSql.sql
@@ -3,3 +3,8 @@ Alter Table dbo.t_result_check_item Add Id Int Identity(1, 1)
 SELECT *  FROM Obecnosc where userid = 37 order by dzien desc;
 update dbo.Obecnosc set RogerDateOut = CONVERT(DATETIME, '2021-05-27 15:07:23', 120)  where ID = 58514;
 convert(DATETIME, date , x)  x - wzór daty lista wzorów na - http://www.sqlusa.com/bestpractices/datetimeconversion/
+
+# ilość pracownikuów na terenei zakładu
+SELECT count(*)
+  FROM [ASERO].[dbo].[Obecnosc]
+  where RogerDateIn > CONVERT(DATETIME, '2021-09-28 04:00:00', 120) and ( RogerDateOut is NULL or RogerDateOut > CONVERT(DATETIME, '2021-09-28 10:00:00', 120));
